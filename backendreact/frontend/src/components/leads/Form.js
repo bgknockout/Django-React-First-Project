469885588d99de1ed596e510e2299409300fd4e7
bgknockout <feliperoman3601@gmail.com>
2019-10-30 16:39:11 -0300

    React Template

---
+++ backendreact/frontend/src/components/leads/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Añadir Lead Form </h1>
+            </div>
+        )
+    }
+}
+
+export default Form

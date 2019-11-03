3a521363324e027fdabbb8f7f1c663ef10b381ee
bgknockout <feliperoman3601@gmail.com>
2019-11-03 16:17:43 -0300

    AUTH TOKENS

--- backendreact/frontend/src/actions/leads.js
+++ backendreact/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { createMessage } from './messages';
+import { createMessage, returnErrors } from './messages';
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
 
@@ -16,7 +16,8 @@ export const getLeads = () => dispatch => {
 
             });
         })
-        .catch(err => console.log(err));
+        .catch(err => dispatch(returnErrors(
+            err.response.data, err.response.status)));
 };
 
 export const deleteLead = (id) => dispatch => {
@@ -44,14 +45,6 @@ export const addLead = lead => dispatch => {
 
             });
         })
-        .catch(err => {
-            const errors = {
-                msg: err.response.data,
-                status: err.response.status
-            }
-            dispatch({
-                type: GET_ERRORS,
-                payload: errors
-            })
-        });
+        .catch(err => dispatch(returnErrors(
+            err.response.data, err.response.status)));
 };
--- backendreact/leads/api.py
+++ backendreact/leads/api.py
@@ -2,9 +2,16 @@ from leads.models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
+
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
-    permissions_classes = [
-        permissions.AllowAny
+    permission_classes = [
+        permissions.IsAuthenticated,
     ]
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
